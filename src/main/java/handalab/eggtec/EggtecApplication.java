160351a5af5bac359372d253bac89f6ab5fd37c0
zeroOne0012 <012012012il@gmail.com>
2025-02-05 10:09:07 +0900

    first commit

---
+++ src/main/java/handalab/eggtec/EggtecApplication.java
@@ -0,0 +1,13 @@
+package handalab.eggtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggtecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handalab/eggtec/EggtecApplicationTests.java
@@ -0,0 +1,13 @@
+package handalab.eggtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggtecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
